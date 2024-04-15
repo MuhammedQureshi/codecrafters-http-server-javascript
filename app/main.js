8c3c389f14f1d18dbda6e36450ad37637ffd69ff
LilAliTech <lilalix20@gmail.com>
2024-04-15 18:26:41 +0100

    Respond with 200 OK

--- app/main.js
+++ app/main.js
@@ -5,10 +5,15 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
+    
+    socket.on("data", () => {
+        socket.end("HTTP/1.1 200 OK\r\n\r\n");
+        });
+
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
 });
 
 server.listen(4221, "localhost");
