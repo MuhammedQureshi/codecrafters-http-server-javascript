0f91c6cf20d20bacb3dd735ef849ae2d8ec86e2e
LilAliTech <lilalix20@gmail.com>
2024-04-15 18:37:19 +0100

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -5,10 +5,22 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-    
+
     socket.on("data", () => {
         socket.end("HTTP/1.1 200 OK\r\n\r\n");
         });
+
+    socket.on("data", (data) => {
+        const requestData = data.toString().split("\r\n");
+        const requestPath = requestData[0].split(" ");
+        if (requestPath[1] === "/") {
+            socket.write("HTTP/1.1 200 OK\r\n\r\n");
+        } else {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+
+        socket.end();
+    })
 
     socket.on("close", () => {
         socket.end();
