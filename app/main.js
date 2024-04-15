0372cf754855159620cc039f8f66519c08c84b83
LilAliTech <lilalix20@gmail.com>
2024-04-15 18:45:34 +0100

    Respond with 404

--- app/main.js
+++ app/main.js
@@ -1,28 +1,32 @@
 const net = require("net");
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
-const HTTP_STATUS = {
-  200: "HTTP/1.1 200 OK\r\n\r\n",
-  404: "HTTP/1.1 404 NOT FOUND\r\n\r\n"
-}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
-  socket.on("data", (data) => {
-    const path = data.toString().split(" ")[1];
-    switch(path) {
-      case "/":
-        socket.write(HTTP_STATUS[200]);
-        break;
-      default:
-        socket.write(HTTP_STATUS[404]);
-    }
-1
-  });
-  socket.on("close", () => {
-    socket.end();
-    server.close();
-  });
-});
-server.listen(4221, "localhost");
 
+    socket.on("data", () => {
+        socket.end("HTTP/1.1 200 OK\r\n\r\n");
+        });
+
+  socket.on("data", (data) => {
+    const requestData = data.toString().split("\r\n");
+    const requestPath = requestData[0].split(" ");
+    if (requestPath[1] === "/") {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+    }
+    1;
+1
+    socket.end();
+  });
+
+    socket.on("close", () => {
+        socket.end();
+        server.close();
+    });
+});
+
+server.listen(4221, "localhost");
