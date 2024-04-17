56bd49e2b577bd9194399201842ac793d4b3bfad
LilAliTech <lilalix20@gmail.com>
2024-04-17 14:27:44 +0100

    Parse Headers

--- app/main.js
+++ app/main.js
@@ -51,7 +51,7 @@ function handleAgentRequest(headers, socket) {
         }
     }
 
-    const contentLength = Buffer.byteLength(userAgent, 'utf-8');
+    const contentLength = userAgent -2;
 
     const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${contentLength}\r\n\r\n${userAgent}\r\n`
 
