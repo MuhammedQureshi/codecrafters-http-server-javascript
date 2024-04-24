ad6f556f584bc3b8b20df3f62a7644fe0ce9de83
LilAliTech <lilalix20@gmail.com>
2024-04-24 20:12:43 +0100

    Post a file

--- app/main.js
+++ app/main.js
@@ -1,12 +1,23 @@
 const net = require("net");
 const fs = require("fs");
+const { argv } = require('process');
+const directory = argv[argv.indexOf('--directory') + 1];
 
 console.log("Logs from your program will appear here!");
+
+function parseInput(data) {
+    let arr = data.split('\r\n');
+    return arr;
+}
 
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const [requestLine, ...headers] = data.toString().split("\r\n");
         const [method, path] = requestLine.split(" ");
+        let dataArr = parseInput(Buffer.from(data).toString());
+        const body = dataArr[dataArr.length - 1];
+
+        console.log(data.toString());
 
         if (path === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
@@ -15,7 +26,13 @@ const server = net.createServer((socket) => {
         } else if (path.startsWith('/user-agent')) {
             handleAgentRequest(headers, socket);
         } else if (path.startsWith('/files/')) {
-            handleFileRequest(path, socket);
+            let filename = path.slice(7);
+            if (method === 'GET') {
+                handleFileRequest(path, socket);
+            } else if (method === 'POST') {
+                fs.writeFileSync(`${directory}/${filename}`, body);
+                socket.write('HTTP/1.1 201 OK\r\n\r\n');
+            }
         }
          else {
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
@@ -95,3 +112,19 @@ function handleFileRequest(path, socket) {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 }
+
+// function handlePostRequest(path, data, socket) {
+    
+
+
+//     // Extract the filename from the path
+//     let filename = path.slice(7);
+
+//     // Construct the full path to the file based on the directory provided
+//     const directory = process.argv[process.argv.indexOf("--directory") + 1];
+
+//     fs.writeFileSync(`${directory}/${filename}`, body);
+
+//     socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
+
+// }
